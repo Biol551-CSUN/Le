44beb580520f2cb4425ba3d315357cf3a3df1bba
vivvle <vivianvnle@gmail.com>
2022-02-15 11:08:22 -0800

    review the script and add comments

--- Week_3/Scripts/week3scriptlab.R
+++ Week_3/Scripts/week3scriptlab.R
@@ -20,13 +20,17 @@ view(penguins)
 ggplot(data=penguins,
        mapping = aes(x = species,
                      y = body_mass_g)) +
-  geom_boxplot(aes(fill = species)) +
-  geom_dotplot(binaxis = "y",
+  geom_boxplot(aes(fill = species)) + # boxplot function
+  geom_dotplot(binaxis = "y", # this adds dots to the plot overlay the boxplot
                dotsize = 0.2,
-               stackdir = "center") +
-  scale_fill_manual(values= beyonce_palette(39)) +
+               stackdir = "center") + # this centers the dots in the graph
+  scale_fill_manual(values= beyonce_palette(39)) + # this changes the color to the boxplots but not the dots
   labs(title = "Body Mass Across Penguin Species", x = "Species", y = "Body Mass (g)") +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text = element_text(size = 10),
-        panel.border = element_rect(fill = NA, color = "black"))
+        panel.border = element_rect(fill = NA, color = "black")) # this adds a border to the entire graph
+
+# ggsave saves to the computer
+# do not add a plus to the last function to save
+# save is a separate function
 ggsave(here("Week_3","output","body_mass_pens.png"), width = 7, height = 6)
