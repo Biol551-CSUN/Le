a5fa51f8dad0b41762f1ea12a38fa14e45132fba
vivvle <vivianvnle@gmail.com>
2022-04-05 14:53:38 -0700

    uploading the render error script from tuesday lab

---
+++ Week_10/Scripts/week10_tues_lab.R
@@ -0,0 +1,15 @@
+### The REAL Group 5
+### Sophia Boyd, Brandon Bonilla, Alex Dang, Vivian Vy Le
+
+library(tidyverse)
+library(palmerpenguins)
+
+penguin %>%
+  ggplot(aes(x= bill_depth_mm,
+             y = bill_length_mm,
+             color = islands)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  labs(title = "Comparing Penguins Length and Depth",
+       x = "Depth (mm)",
+       y = "Length (mm)")
