de0fa6b29d117653254254581cf597b42414a0a3
vivvle <vivianvnle@gmail.com>
2022-04-07 01:11:36 -0700

    uploading script with solution from group 2

--- Week_10/Scripts/week10_tues_lab.R
+++ Week_10/Scripts/week10_tues_lab.R
@@ -13,3 +13,19 @@ penguin %>%
   labs(title = "Comparing Penguins Length and Depth",
        x = "Depth (mm)",
        y = "Length (mm)")
+
+
+#Group 2's response to Group 5
+#penguin should be penguins, islands should be island
+library(tidyverse)
+library(palmerpenguins)
+
+penguins %>%
+  ggplot(aes(x= bill_depth_mm,
+             y = bill_length_mm,
+             color = island)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  labs(title = "Comparing Penguins Length and Depth",
+       x = "Depth (mm)",
+       y = "Length (mm)")
